6c4393737b743b474d39f5ea0d031526dd77357b
TioFlavin <92895161+TioFlavin@users.noreply.github.com>
2023-05-08 15:55:55 -0300

    Add files via upload

---
+++ script.py
@@ -0,0 +1,46 @@
+StartTxT = """
+Olá {mention}
+
+Sou um bot de filtro avançado com muitos recursos!
+
+Não há limites práticos para minha capacidade de filtragem :)
+
+Veja o botão de ajuda para comandos e mais detalhes
+"""
+
+HelpTxT = """
+Adicione-me como administrador em seu grupo e comece a filtrar :)
+
+<b>Comandos Básicos;</b>
+
+/start - Verifique se estou vivo! 
+/help - Comandos de Ajuda
+/about - Sobre mim
+
+<b>Filter Commands;</b>
+
+<code>/add Nome do Filme</code>  -  Adicionar filtro para nome
+<code>/del name</code>  -  Delete filter
+<code>/delall</code>  -  Excluir filtros inteiros (Somente Proprietário do Grupo!)
+<code>/viewfilters</code>  -  Listar todos os filtros no chat
+
+<b>Comandos de conexão;</b>
+<code>/connect groupo id</code>  -  Conecte seu grupo ao meu Pv. Você também pode simplesmente usar,
+<code>/connect</code> Em Grupos.
+<code>/connections</code>  -  Gerencie suas conexões.
+
+<b>Extras;</b>
+/status  -  Mostra o status atual do seu bot (somente usuário de autenticação)
+/id  -  Mostra informações de identificação
+/info userid  -  Mostra informações do usuário. Use <code>/info</code> como resposta a alguma mensagem para seus detalhes!
+
+© @TioFlixBR
+"""
+
+AboutTxT = """
+⭕️ My Name : FlixGo Bot</b>
+⭕️ Developers : @TioFlixBR 
+⭕️ Upgrader : @FlixGoOficial
+⭕️ Language : Python3
+⭕️ Library : <a href='https://docs.pyrogram.org/'>Pyrogram 2.0.102</a> 
+"""
